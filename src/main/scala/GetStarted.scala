1c4c8c0c596fb02f76323df9be4ee9ca753f5dbf
Frank Tan <frank.tan@rea-group.com>
2017-11-11 10:14:02 +1100

    scaffolding the project

---
+++ src/main/scala/GetStarted.scala
@@ -0,0 +1,7 @@
+object GetStarted extends App {
+
+  def square(x: Int) = x * x
+
+  println("Hello, World!!")
+  println("Hi Frank")
+}
---
+++ src/test/scala/GetStartedTest.scala
@@ -0,0 +1,9 @@
+import org.scalatest.FunSuite
+
+class GetStartedTest extends FunSuite {
+
+  test("testSquare") {
+    assert(GetStarted.square(3) === 9)
+  }
+
+}
