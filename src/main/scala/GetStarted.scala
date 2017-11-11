7bbef8937d6eeb2cb2a6d2f8fdb4f7aef0d2bd83
Frank Tan <frank.tan@rea-group.com>
2017-11-11 11:29:21 +1100

    create a tail recursive function to get Fibonacci number by index

--- src/main/scala/GetStarted.scala
+++ src/main/scala/GetStarted.scala
@@ -2,6 +2,16 @@ object GetStarted extends App {
 
   def square(x: Int) = x * x
 
-  println("Hello, World!!")
-  println("Hi Frank")
+  def getFibonacci(index: Int): Int = {
+    @annotation.tailrec
+    def getTailRec(index: Int, prev: Int, current: Int): Int = {
+      if (index <= 0) {
+        current
+      } else {
+        getTailRec(index - 1, prev = prev + current, current = prev)
+      }
+    }
+
+    getTailRec(index, 1, 0)
+  }
 }
--- src/test/scala/GetStartedTest.scala
+++ src/test/scala/GetStartedTest.scala
@@ -6,4 +6,15 @@ class GetStartedTest extends FunSuite {
     assert(GetStarted.square(3) === 9)
   }
 
+  test("getFibonacci") {
+    assert(GetStarted.getFibonacci(0) === 0)
+    assert(GetStarted.getFibonacci(1) === 1)
+    assert(GetStarted.getFibonacci(2) === 1)
+    assert(GetStarted.getFibonacci(3) === 2)
+    assert(GetStarted.getFibonacci(4) === 3)
+    assert(GetStarted.getFibonacci(6) === 8)
+    assert(GetStarted.getFibonacci(10) === 55)
+    assert(GetStarted.getFibonacci(12) === 144)
+  }
+
 }
